f52d5a3e86e6b4f2c2d3f9657a4a2dacd66d867e
Oriana Benitez <orianabenitez6227@gmail.com>
2024-02-01 12:31:39 -0400

    new outputs with reduced data

--- interpolacion_reduction.py
+++ interpolacion_reduction.py
@@ -9,37 +9,37 @@ data = pd.read_csv('anomalia_airelibre_5arcmin.csv')
 
 data = data.where(data['lon']>-72).dropna()
 
-projection = pyproj.Proj(proj="utm",zone=19, ellps='WGS84', preserve_units=False)
-x,y = projection(data.lon, data.lat)
+#projection = pyproj.Proj(proj="utm",zone=19, ellps='WGS84', lat_ts=data.lat.mean())
+#x,y = projection(data.lon, data.lat)
+#coordinates = (x,y)
 
-coordinates = (x,y)
 region=vd.get_region((data.lon,data.lat))
 spacing = 1/60
 
 reducer = vd.BlockReduce(reduction=np.average, spacing=spacing,adjust='region')
 coordinates, anom_al = reducer.filter((data.lon, data.lat), data.anom_al_mgal)
 
-projection = pyproj.Proj(proj="merc", lat_ts=data.lat.mean())
+projection = pyproj.Proj(proj='merc', ellps='WGS84', lat_ts=data.lat.mean())
 proj_coordinates = projection(*coordinates)
 
 ## Linear interpolation
-grd = vd.Linear().fit(proj_coordinates,anom_al)
-grid = grd.grid(region=region,spacing=spacing, projection=projection, dims=['lat','lon'], data_names="anom_al_mgal")
+# grd = vd.Linear().fit(proj_coordinates,anom_al)
+# grid = grd.grid(region=region,spacing=spacing, projection=projection, dims=['lat','lon'], data_names="anom_al_mgal")
 
 ##nearest neighbors interpolation, k being number of neighbors to consider
-# grd = vd.KNeighbors(k=10)
-# grd.fit(coordinates, data.anom_al_mgal)
+# grd = vd.KNeighbors(k=5)
+# grd.fit(proj_coordinates,anom_al)
 # grid = grd.grid(region=region, spacing=spacing, projection=projection,dims=['lat','lon'], data_names="anom_al_mgal")
 
 ##cubic interpolation
-# grd = vd.Cubic().fit(coordinates, data.anom_al_mgal)
+# grd = vd.Cubic().fit(proj_coordinates,anom_al)
 # grid = grd.grid(region=region, spacing=spacing, projection=projection,dims=['lat','lon'], data_names="anom_al_mgal")
 
-# interpolation using splines (not enough ram to 1arcmin)
-# damping parameter obtained by cross-validated spline (see Verde SplineCV documentation) with least R squared
-# spline = vd.SplineCV()
-# spline.fit(coordinates, data.anom_al_mgal)
-# grid = spline.grid(region=region, spacing=spacing, projection=projection,dims=['lat','lon'], data_names="anom_al_mgal")
+## interpolation using splines (not enough ram to 1arcmin)
+## damping parameter obtained by cross-validated spline (see Verde SplineCV documentation) with least R squared
+spline = vd.Spline(damping=1e-10)
+spline.fit(proj_coordinates,anom_al)
+grid = spline.grid(region=region, spacing=spacing, projection=projection,dims=['lat','lon'], data_names="anom_al_mgal")
 
 crs = ccrs.PlateCarree()
 
@@ -49,5 +49,5 @@ ax.coastlines()
 pc = grid.anom_al_mgal.plot.pcolormesh(ax=ax,transform=crs, zorder=-1, cmap = 'seismic', add_colorbar=False)
 plt.colorbar(pc).set_label('mgal')
 
-#plt.savefig('test_reduced')
+plt.savefig('outputs/spline_reduced')
 plt.show()
